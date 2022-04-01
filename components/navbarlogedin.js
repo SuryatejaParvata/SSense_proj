d431c4d45cf42e8ee490cde4aab833df03f34353
SuryatejaParvata <96101598+SuryatejaParvata@users.noreply.github.com>
2022-04-01 21:32:29 +0530

    first commit

---
+++ components/navbarlogedin.js
@@ -0,0 +1,22 @@
+const navbar=()=>{
+
+    return `<div id="navbar">
+    <div id="left">
+        <a href="">MENSWEAR</a>
+        <a href="">WOMENSWEAR</a>
+        <a href="">EVERYTHING ELSE</a>
+        <a href="">SEARCH</a>
+    </div>
+    
+        <a href="landingpage.html" ><img src="https://res.cloudinary.com/ssenseweb/image/upload/v1471963917/web/ssense_logo_v2.svg" alt="" id="logo"></a>
+
+        <div id="right">
+            <a href="">ENGLISH</a>
+            <a href="">ACCOUNT</a>
+            <a href="">WISHLIST</a>
+            <a href="">SHOPPING BAG</a>
+        </div>
+</div>`;
+}
+
+export default navbar
